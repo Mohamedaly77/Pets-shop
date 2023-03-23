a6df681a1986d236980ce52e67e8bcddf941c033
Mohamed-aly77 <125808033+Mohamed-aly77@users.noreply.github.com>
2023-03-23 20:11:50 +0200

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import cart from '../pets assets/cart.svg';
+import login from '../pets assets/login.svg';
+import menu from '../pets assets/Hamburger menu.svg';
+import logo from '../pets assets/logo.svg';
+
+import { navLinks } from '../constants';
+import search from '../pets assets/seach.svg';
+function Navbar() {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <nav className={`flex justify-between w-full px-2 py-2 relative`}>
+      <div className=" flex  justify-start items-center ">
+        <img
+          src={logo}
+          alt="logo "
+          className=" w-[45px] h-[47px] object-contain "
+        />
+        <h4 className=" text-[16px] ml-4 font-normal font-cabin">PucciMart</h4>
+      </div>
+
+      <ul className=" flex-1 justify-center items-center md:flex hidden ">
+        {navLinks.map((nav) => (
+          <li className=" mr-4 font-lato font-normal text-[16px] text_black cursor-pointer hover:text-primary_green leading-[19.2px]">
+            <a href={nav.id}>{nav.title}</a>
+          </li>
+        ))}
+        <form className=" w-[261px] h-[30px]">
+          <div className=" flex justify-center items-center ">
+            <input
+              type="text"
+              placeholder="Search for products ..."
+              className=" border black rounded-xl px-3  font-lato font-normal  w-[261px] h-[30px] text-[10px]"
+            />
+
+            <button type="Submit" className=" -ml-7 ">
+              <img src={search} alt="search" className=" w-[20px] h-[20px]" />
+            </button>
+          </div>
+        </form>
+      </ul>
+
+      <div className=" flex justify-end px-3 cursor-pointer items-center">
+        <img className=" object-contain ml-4" src={cart} alt="cart" />
+        <img className="  object-contain ml-4" src={menu} alt="menu" />
+        <img className="  object-contain ml-4" src={login} alt="login" />
+      </div>
+
+      <div className=" absolute w-[1080px] h-[1px] bg-primary_green bottom-0  left-20 mt-7"></div>
+    </nav>
+  );
+}
+
+export default Navbar;
